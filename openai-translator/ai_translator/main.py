9bcab0ec179a41f35916c35bbe00f5a580f82ebb
Richard Li <RICHARD.X.S.LI@OOCL.COM>
2024-07-24 09:30:21 +0800

    day2 homework:2 ai-translator

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -7,6 +7,9 @@ from utils import ArgumentParser, ConfigLoader, LOG
 from model import GLMModel, OpenAIModel
 from translator import PDFTranslator
 
+os.environ['OPENAI_API_KEY'] = 'sk-hXK719WAh86MxL6u2877B33101154f6cAcDdC89e1889F551'
+os.environ['OPENAI_BASE_URL'] = 'https://api.xiaoai.plus/v1'
+
 if __name__ == "__main__":
     argument_parser = ArgumentParser()
     args = argument_parser.parse_arguments()
@@ -24,4 +27,5 @@ if __name__ == "__main__":
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
+    translator.translate_pdf(pdf_file_path, file_format, 'Korean')
     translator.translate_pdf(pdf_file_path, file_format)
